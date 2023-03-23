76f1a211fafa9375f6b8de7fd5ebe1ab5b27c89c
Dan Herbert <vector2022plusplus-dan@hrbrt.co>
2023-03-23 10:22:53 -0700

    Fix bug in presubmit for css regeneration
    The "prettier" command had the wrong file and therefore wasn't actually
    formatting the generated CSS

--- .husky/regenerate-css.sh
+++ .husky/regenerate-css.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-set -eux
+set -eu
 
 if git status -s | grep 'vector2022pp.scss$'; then
-  fullPath=$(git status -s | grep '^M.\+vector2022pp.scss$' | sed -E 's/^M\s+//')
-  filename=$(basename $fullPath)
-  absoluteDir=$(cd $(dirname ${fullPath}) && pwd)
-  absolutePath="$absoluteDir/$filename"
-  sass $absolutePath ${absolutePath/scss/css}
-  prettier --print-width 100 --write $absolutePath
-  git add ${absolutePath/scss/css} ${absolutePath/scss/css}.map -f
+  relativeSassPath=$(git status -s | grep '^M.\+vector2022pp.scss$' | sed -E 's/^M\s+//')
+  sassFilename=$(basename $relativeSassPath)
+  absoluteSassDir=$(cd $(dirname ${relativeSassPath}) && pwd)
+  absoluteSassPath="$absoluteSassDir/$sassFilename"
+  absoluteCssPath="${absoluteSassPath/scss/css}"
+  npx sass $absoluteSassPath $absoluteCssPath
+  npx prettier --print-width 100 --write $absoluteCssPath
+  git add $absoluteCssPath $absoluteCssPath.map -f
 fi
 
 exit 0
